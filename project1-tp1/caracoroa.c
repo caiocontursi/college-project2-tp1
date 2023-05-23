5bb98c9708194d9f057588c3d053939f3e7d0113
Caio <c.fernandescontursi@gmail.com>
2023-05-22 23:35:46 -0300

    second part

--- project1-tp1/caracoroa.c
+++ project1-tp1/caracoroa.c
@@ -1,3 +1,5 @@
+// Jogo de cara ou coroa. Segunda questão do Trabalho de Técnicas de Programação I, primeiro período 2023.
+
 #include <stdio.h>
 
 int main() {
@@ -22,21 +24,25 @@ while(1) {
 
     int vitmaria = 0, vitjoao = 0;
 
-        for(contador = 1; contador <= numjogos; contador = contador + 1) {
+        for(contador = 1; contador <= numjogos; contador++) {
             scanf("%d", &resultado);
 
-                if(resultado == 0)
-                    vitmaria = vitmaria + 1;
+                if(resultado != 0 && resultado != 1) {
+                    printf("Jogo abortado! Digite somente 1 ou 0 \n");
+                    return main();
+                }
+                else 
+                {
+                    if(resultado == 0)
+                        vitmaria++;
 
-                if(resultado == 1)
-                    vitjoao = vitjoao + 1;
-
-                if(resultado != 1 && resultado != 0)
-                    printf("ERRO! Digite somente 1 ou 0 \n");
-                
+                    if(resultado == 1)
+                        vitjoao++;
+                }
+                    
         }
-            printf("Maria ganhou %d vez(es) e Joao ganhou %d vez(es)! \n", vitmaria, vitjoao);
+        printf("Maria ganhou %d vez(es) e Joao ganhou %d vez(es)! \n", vitmaria, vitjoao);
 
+return 0;
 }
-        return 0;
 }
